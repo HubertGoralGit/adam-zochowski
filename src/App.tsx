ed425c4ecdf5889d9925e2557649ad0adf0108b1
HubertGoralGit <h.goral95@gmail.com>
2022-12-27 21:00:22 +0100

    add theme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { Home } from './Pages/Home';
 import { About } from './Pages/About';
 import { Contact } from './Pages/Contact';
 import { Projects } from './Pages/Projects';
+import { theme } from './theme';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
